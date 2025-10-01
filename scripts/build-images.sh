6b2edf89e86f66909d5fcbc3e51565a784c5fa28
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-10-01 18:48:18 +0100

    Update build commands to reference PolicyWorkflow instead of Car_central_control

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo podman build -t localhost/policy-workflow:v0.0.1 ./Car_central_control
+sudo podman build -t localhost/policy-workflow:v0.0.1 ./PolicyWorkflow
 sudo podman build -t localhost/emergency-brake:v0.0.1 ./EmergencyBrake_v1 
 sudo podman build -t localhost/emergency-brake:v0.0.2 ./EmergencyBrake_v2 
