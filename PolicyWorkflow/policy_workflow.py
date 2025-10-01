1fa5b7c769ddf200ce702eec078314dbd9dc435d
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-10-01 23:23:34 +0100

    Update rootResource in update.json to 'update' for consistency

--- PolicyWorkflow/policy_workflow.py
+++ PolicyWorkflow/policy_workflow.py
@@ -66,10 +66,6 @@ def update_possible(body: dict = Body(..., description="Subset of required statu
     popup_res.raise_for_status()
 
     return 200
-
-
-    
-
     
 @app.post("/update")
 def update(body: dict = Body(..., description="Target to be published to the symphony api")):
