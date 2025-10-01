017f00898a46d16412c80dfa5f38be0434388b20
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-10-01 12:10:33 +0100

    Add script to register campaigns with Symphony API

---
+++ scripts/register-campaigns.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export SYMPHONY_API_URL=http://localhost:8082/v1alpha2/
+
+TOKEN=$(curl -X POST -H "Content-Type: application/json" -d '{"username":"admin","password":""}' "${SYMPHONY_API_URL}users/auth" | jq -r '.accessToken')
+
+curl -X POST \
+  -H "Authorization: Bearer $TOKEN" \
+  -H "Content-Type: application/x-yaml" \
+  --data-binary @../workflows/update.yaml \
+  http://localhost:8082/v1alpha2/campaigns/registry/update
+
