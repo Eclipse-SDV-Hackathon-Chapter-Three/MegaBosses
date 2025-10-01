adf717d08b8a983f2c09c68fd73962916d6abff3
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-10-01 11:57:53 +0100

    Refactor truck detection function and add update-trigger to workflow configuration

--- ManagementPlatform/campain.py
+++ ManagementPlatform/campain.py
@@ -4,9 +4,6 @@ import json
 symphony_base_url = "http://localhost:8080/v1alpha2"
 
 def truck_detected():
-    print("truck detected")
-
-    # Authenticate and get the token
     auth_response = requests.post(
         f"{symphony_base_url}/users/auth",
         headers={"Content-Type": "application/json"},
